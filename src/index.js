0c95b6661687ec088f053a1945781843deadf0d1
SprengerV <b.francis@sprengerv.com>
2021-03-27 10:13:43 -0400

    merge with main

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express()
 
 // configure session storage
 app.use(cookieSession({
-    name: 'session-name',
-    keys: ['key1', 'key2']
+  name: 'session-name',
+  keys: ['key1', 'key2']
 }))
 
 // configure passport
@@ -17,40 +17,40 @@ app.use(passport.session())
 
 // unprotected routes
 app.get('/', (req, res) => {
-    // HOME PAGE GOES HERE
-    // res.send()
+  // HOME PAGE GOES HERE
+  // res.send()
 })
 
 app.get('/failed', (req, res) => {
-    // LOG IN FAILED PAGE GOES HERE
-    // res.send()
+  // LOG IN FAILED PAGE GOES HERE
+  // res.send()
 })
 
 // middleware to check if user is logged in
 const checkUserLoggedIn = (req, res, next) => {
-    req.user ? next() : res.sendStatus(401)
+  req.user ? next() : res.sendStatus(401)
 }
 
 // protected route
 app.get('/profile', checkUserLoggedIn, (req, res) => {
-    // LOGGED IN PROFILE PAGE GOES HERE
-    // res.send('<h1>req.user.displayName</h1>)
+  // LOGGED IN PROFILE PAGE GOES HERE
+  // res.send('<h1>req.user.displayName</h1>)
 })
 
 // auth routes
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
-    function (req, res) {
-        res.redirect('/profile');
-    }
+  function (req, res) {
+    res.redirect('/profile')
+  }
 )
 
 // logout
 app.get('/logout', (req, res) => {
-    req.session = null
-    req.logout()
-    res.redirect('/')
+  req.session = null
+  req.logout()
+  res.redirect('/')
 })
 
 app.listen(3000, () => console.log('App listening on port 3001'))
