d70d7bcf9f96ef3fc837d148f1c25f27b2a85341
Taylor Gonzales <hello@taylorgonz.com>
2021-03-27 14:10:22 -0400

    classList javascript function

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+classArray = [
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+]
+
+const classList = () => {
+    classArray.forEach(index => {
+       
+    })
+}
+classList();
