29dade3eae16efbbfc4eaf4c3d1ce9b17a8bd8e4
SprengerV <b.francis@sprengerv.com>
2021-03-31 19:21:04 -0400

    working on passport functionality

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const LocalStrategy = require('passport-local').Strategy
+const bCrypt = require('bcrypt-nodejs')
+const User = require('../models').User
+
 
 passport.serializeUser(function (user, done) {
   done(null, user)
@@ -10,6 +14,7 @@ passport.deserializeUser(function (user, done) {
   done(null, user)
 })
 
+// Oauth2.0 Google strategy
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -19,3 +24,44 @@ function (accessToken, refreshToken, profile, cb) {
   return cb(null, profile)
 }
 ))
+
+// Local signup
+passport.use('local-signup', new LocalStrategy(
+  {
+    usernameField: 'user_name',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  (req, user_name, password, done) => {
+    const generateHash = (password) => {
+      return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null)
+    }
+    User.findOne({
+      where: {
+        user_name: user_name
+      }
+    }).then((user) => {
+      if (user) {
+        return done(null, false, {
+          message: 'That user name is already taken'
+        })
+      } else {
+        const userPassword = generateHash(password)
+        const data = 
+          {
+            user_name: user_name,
+            password: userPassword
+          }
+        User.create(data).then((newUser, created) => {
+          if (!newUser) {
+            return done(null, false)
+          }
+          if (newUser) {
+            return done(null, newUser)
+          }
+        })
+      }
+    })
+  }
+))
+
